f2c235ea4c839e7bed1c2e4ba750b71f85c66c67
meiyrbek4 <meiyrbek@gmail.com>
2025-01-30 15:10:17 +0500

    added PWA

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,10 @@ import './index.css';
 import { Provider } from 'react-redux';
 import store from './store';
 
+import {registerSW} from "virtual:pwa-register";
+
+registerSW({immediate: true})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,53 @@ import react from '@vitejs/plugin-react';
 // import eslint from "vite-plugin-eslint";
 import tainwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
+import {ManifestOptions, VitePWA} from "vite-plugin-pwa"
+
+
+const manifest: Partial<ManifestOptions> = {
+  "theme_color":"#ffcf66",
+  "background_color":"#ffffff",
+  "icons":[{
+    "purpose":"maskable",
+    "sizes":"512x512",
+    "src":"icon512_maskable.png",
+    "type":"image/png"
+  },
+  {
+    "purpose":"any",
+    "sizes":"512x512",
+    "src":"icon512_rounded.png",
+    "type":"image/png"
+  }],
+  "screenshots": [{
+    "src": "/screenshots/desktop.png",
+    "type": "image/png",
+    "sizes": "2880x1550",
+    "form_factor": "wide",
+  },
+  {
+    "src": "/screenshots/mobile.png",
+    "type": "image/png",
+    "sizes": "794x1376",
+    "form_factor": "narrow",
+  }],
+  "orientation":"any",
+  "display":"standalone",
+  "lang":"en",
+  "name":"Fast React Pizza",
+  "short_name":"RPizza",
+  "start_url":"/"
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    registerType: "autoUpdate",
+    workbox: {
+      globPatterns: ["**/*.{html,css,js,svg,ico,png,jpg}"]
+    },
+    manifest: manifest,
+  })],
   css: {
     postcss: {
       plugins: [tainwindcss(), autoprefixer()]
